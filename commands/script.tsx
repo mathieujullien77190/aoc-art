7b29ad2293390d664f01d55f4df6a1876f4f3ac1
mathieujullien77190 <mathieu.jullien77190@gmail.com>
2022-11-28 01:31:13 +0100

    improve

--- commands/script.tsx
+++ commands/script.tsx
@@ -1,6 +1,8 @@
 /** @format */
 
+import { useState } from "react"
 import Game2021 from "./game/2021"
+import { colors } from "_components/constants"
 
 export const scripts = [
 	{
@@ -12,5 +14,34 @@ export const scripts = [
 ]
 
 export const Games = ({ day }: { day: string }) => {
-	return <>{day === "-1" && <Game2021 />}</>
+	const [display, setDisplay] = useState<boolean>(true)
+
+	const search = scripts.filter(script => script.day === day)
+
+	return (
+		<>
+			{display && search.length === 1 && (
+				<div
+					style={{
+						display: "flex",
+						position: "fixed",
+						justifyContent: "center",
+						alignItems: "center",
+						top: "24px",
+						right: "24px",
+						bottom: "24px",
+						left: "24px",
+						zIndex: 10,
+						background: colors.overlay,
+						border: `solid 2px ${colors.textColor}`,
+						cursor: "pointer",
+						overflow: "hidden",
+					}}
+					onClick={() => setDisplay(false)}
+				>
+					{day === "-1" && <Game2021 />}
+				</div>
+			)}
+		</>
+	)
 }
