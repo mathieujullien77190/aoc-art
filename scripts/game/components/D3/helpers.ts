5948abd26c5c4dfc6f48ecf49f4a7f5b6e7fb37c
mathieujullien77190 <mathieu.jullien77190@gmail.com>
2023-01-08 14:31:59 +0100

    refactor: simplify the 3D helpers

--- scripts/game/components/D3/helpers.ts
+++ scripts/game/components/D3/helpers.ts
@@ -1,41 +1,28 @@
 /** @format */
 
-import { Transform } from "./types"
-
 export const next = (
 	value: number,
 	options: {
 		min: number
 		max: number
-		loop?: boolean
-		withTransition?: boolean
 	},
 	nextValue: number
-): Transform<number> => {
+): { value: number; diff: number } => {
 	if (nextValue > 0) {
 		return {
-			withTransition: !!options.withTransition,
 			value:
 				Math.floor(
-					(value + nextValue > options.max
-						? options.loop
-							? options.min
-							: options.max
-						: value + nextValue) * 10
+					(value + nextValue > options.max ? options.min : value + nextValue) *
+						10
 				) / 10,
 			diff: nextValue,
 		}
 	}
 
 	return {
-		withTransition: !!options.withTransition,
 		value:
 			Math.floor(
-				(value + nextValue < options.min
-					? options.loop
-						? options.max
-						: options.min
-					: value + nextValue) * 10
+				(value + nextValue < options.min ? options.max : value + nextValue) * 10
 			) / 10,
 		diff: nextValue,
 	}
