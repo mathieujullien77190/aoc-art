89425e4b8c06dce3fda3353320d70894fb55a388
mathieujullien77190 <mathieu.jullien77190@gmail.com>
2026-08-24 17:23:26 +0200

    chore: remove dead deps and webpack aliases

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @format */
 
-require("dotenv").config()
-const path = require("path")
-const withPlugins = require("next-compose-plugins")
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
 	enabled: process.env.ANALYZE === "true",
 	openAnalyzer: process.env.ANALYZE === "true",
@@ -18,29 +15,6 @@ const nextConfig = {
 			fileName: false,
 		},
 	},
-
-	exportPathMap: function () {
-		return {
-			"/": { page: "/" },
-		}
-	},
-	webpack: (config, _options) => {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			_: path.join(__dirname),
-			_components: path.join(__dirname, "components"),
-			_commands: path.join(__dirname, "commands"),
-			_games: path.join(__dirname, "games"),
-			_pages: path.join(__dirname, "pages"),
-			_contexts: path.join(__dirname, "contexts"),
-			_hooks: path.join(__dirname, "hooks"),
-			_store: path.join(__dirname, "store"),
-			_constants: path.join(__dirname, "constants"),
-			_public: path.join(__dirname, "public"),
-		}
-		config.resolve.extensions.push(...[".ts", ".tsx"])
-		return config
-	},
 }
 
-module.exports = withPlugins([withBundleAnalyzer], nextConfig)
+module.exports = withBundleAnalyzer(nextConfig)
